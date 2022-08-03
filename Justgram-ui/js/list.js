94004928dc0eb9eb8890a1a7f3f8c6ea59bfdeb2
Cupcakes33 <rute930627@gmail.com>
2022-08-03 16:54:34 +0900

    fix: 이미지 상단 프로필사진 및 유저명 레이아웃

--- Justgram-ui/js/list.js
+++ Justgram-ui/js/list.js
@@ -4,17 +4,17 @@ const $listButton = document.querySelectorAll(".listButton");
 const $CommentForm = document.querySelectorAll(".CommentForm");
 const $cardCommentUl = document.querySelectorAll(".cardCommentUl");
 
+// 폼의 갯수 = 피드의 갯수
 for (let i = 0; i < $CommentForm.length; i++) {
   $CommentForm[i].addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // submit 시에 새로고침을 막음
     const textcontents = $listInputArea[i].value;
     const userName = $userNameArea[i].value;
-
     const templit = `<li><span>${userName}</span>${textcontents}</li>`;
     $cardCommentUl[i].insertAdjacentHTML("beforeend", templit);
     removeTextcontents();
   });
-
+// 댓글이 submit 되면 작성자와 작성한 댓글을 지워준다
   function removeTextcontents() {
     $listInputArea[i].value = "";
     $userNameArea[i].value = "";
@@ -22,6 +22,8 @@ for (let i = 0; i < $CommentForm.length; i++) {
     $listButton[i].classList.remove("active");
   }
 
+// 버튼 활성화 및 비활성화 기능
+// 둘 다 기입했을때만 활성화
   function listButtonControl() {
     if ($listInputArea[i].value === "" || $userNameArea[i].value === "") {
       $listButton[i].disabled = true;
@@ -31,6 +33,7 @@ for (let i = 0; i < $CommentForm.length; i++) {
       $listButton[i].classList.add("active");
     }
   }
+// 두 요소의 input 을 감지하는 이벤트리스너
   $listInputArea[i].addEventListener("input", listButtonControl);
   $userNameArea[i].addEventListener("input", listButtonControl);
 }
