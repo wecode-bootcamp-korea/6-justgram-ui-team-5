305db16b095da13d2b3ad8d2a1c0d26ffdbbf735
Cupcakes33 <rute930627@gmail.com>
2022-08-03 15:36:23 +0900

    Add : 피드별 댓글 & 작성자, 댓글 분리

--- Justgram-ui/js/list.js
+++ Justgram-ui/js/list.js
@@ -1,4 +1,5 @@
-const $listArea = document.querySelectorAll(".listArea");
+const $listInputArea = document.querySelectorAll(".listInputArea");
+const $userNameArea = document.querySelectorAll(".userNameArea");
 const $listButton = document.querySelectorAll(".listButton");
 const $CommentForm = document.querySelectorAll(".CommentForm");
 const $cardCommentUl = document.querySelectorAll(".cardCommentUl");
@@ -6,27 +7,30 @@ const $cardCommentUl = document.querySelectorAll(".cardCommentUl");
 for (let i = 0; i < $CommentForm.length; i++) {
   $CommentForm[i].addEventListener("submit", (e) => {
     e.preventDefault();
-    const textcontents = $listArea[i].value;
-    const templit = `<li><span>우주호</span>${textcontents}</li>`;
+    const textcontents = $listInputArea[i].value;
+    const userName = $userNameArea[i].value;
+
+    const templit = `<li><span>${userName}</span>${textcontents}</li>`;
     $cardCommentUl[i].insertAdjacentHTML("beforeend", templit);
     removeTextcontents();
-
-    
   });
 
   function removeTextcontents() {
-    $listArea[i].value = "";
+    $listInputArea[i].value = "";
+    $userNameArea[i].value = "";
     $listButton[i].disabled = true;
     $listButton[i].classList.remove("active");
   }
 
-  $listArea[i].addEventListener("input", () => {
-    if ($listArea[i].value === "") {
+  function listButtonControl() {
+    if ($listInputArea[i].value === "" || $userNameArea[i].value === "") {
       $listButton[i].disabled = true;
       $listButton[i].classList.remove("active");
     } else {
       $listButton[i].disabled = false;
       $listButton[i].classList.add("active");
     }
-  });
+  }
+  $listInputArea[i].addEventListener("input", listButtonControl);
+  $userNameArea[i].addEventListener("input", listButtonControl);
 }
