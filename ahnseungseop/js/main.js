91749055686a8bb7020993885aa96a97959e86dc
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-05 17:39:23 +0900

    ✨ 메인 UI Mock 데이터 Fetch

--- ahnseungseop/js/main.js
+++ ahnseungseop/js/main.js
@@ -2,6 +2,21 @@ const commentList = document.querySelectorAll(".feed__comment > ul");
 const commentInput = document.querySelectorAll(".feed__comment--input > input");
 const commentBtn = document.querySelectorAll(".feed__comment--input > button");
 
+fetch("data/comments.json", {
+  method: "GET",
+})
+  .then((res) => res.json())
+  .then((data) => {
+    data.feedCommentList.map((commentInfo) => {
+      let newComment = commentInfo.user.username + " " + commentInfo.comment;
+      commentList.forEach((commentListItem) => {
+        const commentListItemChildElement = document.createElement("li");
+        commentListItemChildElement.append(newComment);
+        commentListItem.appendChild(commentListItemChildElement);
+      });
+    });
+  });
+
 Array.from(commentBtn).forEach((button, idx) => {
   button.addEventListener("click", () => {
     const content = commentInput[idx].value;
